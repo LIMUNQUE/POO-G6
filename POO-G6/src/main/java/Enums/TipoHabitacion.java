46af88e247516bc6c7df0d6feea1455e089bca66
“josdramo” <josdramo@espol.edu.ec>
2022-06-22 16:29:48 -0500

    Cree paquetes y algunas clases tambien añadí atributos en la Usuario

---
+++ POO-G6/src/main/java/Enums/TipoHabitacion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Enums;
+
+/**
+ *
+ * @author Dell
+ */
+public enum TipoHabitacion {
+    INDIVIDUAL,DOBLE,FAMILIAR
+}
