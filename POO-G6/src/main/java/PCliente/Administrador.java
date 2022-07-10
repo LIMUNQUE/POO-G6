c9e449fb64a9b7fcbb97ffbf904d0dd7984e432a
MielesGerson <gdmieles@espol.edu.ec>
2022-07-09 20:26:21 -0500

    Administrador consultar reservas

--- POO-G6/src/main/java/PCliente/Administrador.java
+++ POO-G6/src/main/java/PCliente/Administrador.java
@@ -4,6 +4,8 @@
  */
 package PCliente;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Gerson
@@ -22,11 +24,51 @@ public class Administrador extends Usuario{
     public Administrador(String nCedula, String nombre, String apellido, String nombreUsuario, String contrasenia, String nCelular){
         super(nCedula, nombre, apellido, nombreUsuario, contrasenia, nCelular);
     }
-    
-    public void consultarReserva(){
-        System.out.println("/****************Resumen de Servicios Reservados*****************/"
-                       + "\n/*                                                              */"
-                       + "\n/****************************************************************/");
+        /**
+     * Consultar el total de reservas por ciudad y el total general
+     */
+    public void consultarReserva() {
+        System.out.println("""
+                           /****************Resumen de Servicios Reservados*****************/
+                           /*                                                              */
+                           /****************************************************************/""");
+        ArrayList<String> ciudades = new ArrayList<>(); //ArrayList vacio donde agregaré todas las ciudades en dónde existen reservas
+        ArrayList<String> reservas = ManejoArchivos.LeeFichero("reservas.txt"); //lee el archivo de todas las reservas
+
+        for (int i = 1; i < reservas.size(); i++) {
+
+            String verificaCiudad = reservas.get(i).split(",")[7];//
+            if (!ciudades.contains(verificaCiudad)) {
+                ciudades.add(verificaCiudad);
+            }
+        }
+        int sumandoR = 0;
+        for (int j = 0; j < ciudades.size(); j++) {
+            
+            System.out.println("********************" + ciudades.get(j) + "********************");
+            int contRE = 0;
+            int contRH = 0;
+            int contRT = 0;
+            for (int i = 1; i < reservas.size(); i++) {
+                
+                if (ciudades.get(j).equals(reservas.get(i).split(",")[7])) {
+                    if (reservas.get(i).split(",")[2].equals("transporte")) {
+                        contRT++;
+                    } else if (reservas.get(i).split(",")[2].equals("entretenimiento")) {
+                        contRE++;
+                    }else if (reservas.get(i).split(",")[2].equals("hospedaje")){
+                        contRH++;
+                    }
+                }
+            }
+            System.out.println("Entretenmientos reservados: "+contRE);
+            System.out.println("Hospedajes reservados: "+contRH);
+            System.out.println("Transportes reservados: "+contRT);
+            sumandoR+=contRE+contRH+contRT;
+        }
+        System.out.println("***************************************");
+        System.out.println("Reservas totales: "+sumandoR);
+        
     }
     
 }
